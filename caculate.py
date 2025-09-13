7c00fc46ed676e9562c76625d7cbcd2d6a995d06
Pen Menghong <penmenghong7@gmail.com>
2025-09-13 13:37:27 +0700

    push code

---
+++ caculate.py
@@ -0,0 +1,22 @@
+# L = 30
+# W = 20 
+# S = L * W
+# print( "we get S = ", S)
+
+# def caculate(x,y):
+#     return  x * y
+# S = caculate(200,50.30)
+# print("The values is : S =", S)
+
+
+
+# def Age(age) :
+#     return age +1 
+# The_age = Age(17)
+# print("The age Next year is :", The_age)
+
+Money = 10
+if Money >= 500 :
+    print("You are a rich kid = ", Money, "$")
+else :
+    print("B sl o")
---
+++ ruby.rb
@@ -0,0 +1,3 @@
+puts "Input your money that you want to get from me "
+money = gets.chomp
+puts "The that you money have now is: #{money}$ "
